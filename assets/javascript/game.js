4bb9803603cbf39160c2125d332683632f3493b0
yewbdar <32113737+yewbdar@users.noreply.github.com>
2019-02-14 15:48:25 -0600

    Update game.js
    comment added on the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -142,6 +142,6 @@ document.onkeyup = function (e) {
 function playAudio(){
     var audio = new Audio('assets/audio/Drum-loops-and-synth-loops.mp3');
       audio.loop=true;  
-//       audio.volume = 0.2;
+      audio.volume = 2;
       audio.play();
 }
